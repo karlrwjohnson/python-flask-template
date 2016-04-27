3e89d780ca522e959f00ef7f275a428f27ca8ed5
Karl Johnson <karl.rw.johnson@gmail.com>
2016-04-27 18:10:51 -0500

    fix indentation

--- database.py
+++ database.py
@@ -7,31 +7,31 @@ from env import env
 _dbconn = None
 
 def get ():
-    global _dbconn
+  global _dbconn
 
-    if  _dbconn is None:
-        # Configure database connection
-        if env['POSTGRES_CONNECTION_TYPE'] == 'socket':
-          _dbconn = psycopg2.connect(
-            database=env['POSTGRES_DATABASE'],
-            user=env['POSTGRES_USER'],
-            unix_sock='/var/run/postgresql/.s.PGSQL.5432'
-          )
-        elif env['POSTGRES_CONNECTION_TYPE'] == 'network':
-          _dbconn = psycopg2.connect(
-            database=env['POSTGRES_DATABASE'],
-            user=env['POSTGRES_USER'],
-            hostname=env['POSTGRES_HOSTNAME'],
-            port=env['POSTGRES_PORT'] if 'POSTGRES_PORT' in env else 5432,
-          )
-        else:
-            raise RuntimeError('Unknown conneciton type {}'.format(env['POSTGRES_CONNECTION_TYPE']))
+  if  _dbconn is None:
+    # Configure database connection
+    if env['POSTGRES_CONNECTION_TYPE'] == 'socket':
+      _dbconn = psycopg2.connect(
+      database=env['POSTGRES_DATABASE'],
+      user=env['POSTGRES_USER'],
+      unix_sock='/var/run/postgresql/.s.PGSQL.5432'
+      )
+    elif env['POSTGRES_CONNECTION_TYPE'] == 'network':
+      _dbconn = psycopg2.connect(
+      database=env['POSTGRES_DATABASE'],
+      user=env['POSTGRES_USER'],
+      hostname=env['POSTGRES_HOSTNAME'],
+      port=env['POSTGRES_PORT'] if 'POSTGRES_PORT' in env else 5432,
+      )
+    else:
+      raise RuntimeError('Unknown conneciton type {}'.format(env['POSTGRES_CONNECTION_TYPE']))
 
-    return _dbconn
+  return _dbconn
 
 def cursor():
-    return closing(get().cursor())
+  return closing(get().cursor())
 
 def commit():
-    get().commit()
+  get().commit()
 
